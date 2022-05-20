b7efd968f2f72eb6f4f6ddfb13f431a374883c13
Prashik Deshpande <43670200+philomath242@users.noreply.github.com>
2022-05-20 10:58:45 +0530

    Update qrCodeScanner.js

--- src/qrCodeScanner.js
+++ src/qrCodeScanner.js
@@ -15,7 +15,7 @@ qrcode.callback = res => {
     console.log("Output is", res);
     let data = res;
     if (data.includes('upi')){
-      outputData.innerText = data.substring(data.indexOf('pa=') + 3, data.indexOf('&'));
+      afterScan(data);
     }
     else{
       outputData.innerText = data;
@@ -63,3 +63,73 @@ function scan() {
     setTimeout(scan, 300);
   }
 }
+
+
+dragElement(document.getElementById("transaction-text"));
+
+function dragElement(elem) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elem.id + "header")) {
+    document.getElementById(elem.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elem.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();    
+    //pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    //pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    //pos3 = e.clientX;
+    pos4 = e.clientY;
+    elem.style.top = (elem.offsetTop - pos2) + "px";
+    //elem.style.left = (elem.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {  
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function afterScan(data){
+  console.log("here");
+  var dataObject = data.split('&');
+
+  var pa = dataObject[0].split('=').pop();
+  var pn = dataObject[1].split('=').pop();
+  pn = decodeURI(pn);
+
+  outputData.innerText = pa + '<br>' + pn;
+
+  document.getElementById('amount-submit').addEventListener("click", processTransaction(pn));
+
+
+}
+
+function processTransaction(payee) {
+  var amount = document.getElementById('amount-submit').value;
+  if (amount == null){
+    alert("Please enter an amount");
+    processTransaction;
+  }
+  else{
+    document.getElementById('transaction-amount').innerText = amount;
+    document.getElementById('payee-name').innerText = payee;
+    document.getElementById("tone").play();
+    document.getElementById('container').style.display = "none";
+    document.getElementById('transaction-page').style.display = "block";
+    document.body.style.backgroundColor = "var(--green)";
+    document.getElementById('main-video').play();
+  }
+}
+
